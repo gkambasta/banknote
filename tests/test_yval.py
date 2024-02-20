49bc54530ce89bb4c4002add4a3ed52f5daef835
gkambasta <71900334+gkambasta@users.noreply.github.com>
2024-02-20 13:03:54 +0530

    Update test_yval.py

--- tests/test_yval.py
+++ tests/test_yval.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('BankNote_Authentication.csv')
-y = df['class']
-print("value of y =", y[0])
-assert y[0] == 0
+def test_yval():
+  df = pd.read_csv('BankNote_Authentication.csv')
+  y = df['class']
+  print("value of y =", y[0])
+  assert y[0] == 0
